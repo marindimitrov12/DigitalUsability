00c1c86aa013f72e89910e5a8fe4e30599195317
marindimitrov12 <marin200041@gmail.com>
2026-06-06 18:55:48 +0300

    initial commit

--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c7f077d34fd192615e804cb3b34584c45e9598")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61a7132da66c61ee9f1a24365649579e4648305")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
