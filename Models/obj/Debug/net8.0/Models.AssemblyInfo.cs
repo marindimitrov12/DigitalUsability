9f2e76398dbbc4d842e2da44bd51e944d4e3bb29
marindimitrov12 <marin200041@gmail.com>
2026-06-06 18:34:16 +0300

    nmn

--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04cb1508112d239759597713e223998fbbdf15f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6960a4c5e03bb898fccb178c73b31a55df3c710")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
